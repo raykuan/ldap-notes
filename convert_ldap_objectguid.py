71d7fc43c9947dc409e12fc539213faeb0d4c8c9
raykuan <raykuan@qq.com>
2018-04-10 20:02:10 +0800

    Update convert_ldap_objectguid.py

--- convert_ldap_objectguid.py
+++ convert_ldap_objectguid.py
@@ -20,14 +20,14 @@ def guid_to_string(binary_guid):
     return str(uuid.UUID(bytes_le=binary_guid)).lower()
 
 
-################## Important ###############################
-# bytes to hex str
+# 如何通过微软active directory objectGUID 查询对象信息 
+# bytes类型的 objectGUID 与十六进制 str类型 objectGUID 相互转换
 objectGUID = b'Igr\xafb\x19ME\xb2P9c\xfb\xa0\xe2w'
 str_hex_objectGUID = str(uuid.UUID(bytes=objectGUID).hex).upper() # '496772AF62194D45B2503963FBA0E277'
+bytes_objectGUID = uuid.UUID(hex=str_hex_objectGUID).bytes # b'Igr\xafb\x19ME\xb2P9c\xfb\xa0\xe2w'
 
-# str_hex_objectGUID  to a value can use in an ldap search filter
+# str类型 objectGUID 转换为支持 ldap search filter的guid
 guid = ''.join(['\\%s' % str_hex_objectGUID[i:i+2] for i in range(0, len(str_hex_objectGUID), 2)]) # '\\49\\67\\72\\AF\\62\\19\\4D\\45\\B2\\50\\39\\63\\FB\\A0\\E2\\77'
 
 search_filter = ('(objectGUID=%s)' % guid)
-
 ldap.conn.search_s(self.base_dn, user_scope, search_filter)
